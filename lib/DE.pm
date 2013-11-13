0418a6f192673d8fd8c38ebe8cfc5269e81e5ac7
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-11-13 10:54:01 +0700

    Add module: DE

---
+++ lib/DE.pm
@@ -0,0 +1,21 @@
+package DE;
+
+# VERSION
+
+use parent qw(Devel::EndStats);
+
+1;
+# ABSTRACT: Shortcut for Devel::EndStats
+
+=head1 SYNOPSIS
+
+ % perl -MDE -E'...'
+
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+=cut
+
