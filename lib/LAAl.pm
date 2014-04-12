1efcffe92d13be5de404e8b84f7639344129f782
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2014-04-12 08:10:51 +0700

    Add module: LAAl

---
+++ lib/LAAl.pm
@@ -0,0 +1,25 @@
+package LAAl;
+
+use parent qw(Log::Any::App);
+
+# VERSION
+
+sub import {
+    my $caller = caller;
+    Log::Any::App->import('$log');
+    ${"$caller\::log"} = $log;
+}
+
+1;
+# ABSTRACT: Shortcut for Log::Any::App + export $log
+
+=head1 SYNOPSIS
+
+ % LOG_LEVEL=trace perl -MLAAl -E'$log->info("blah"); ...'
+
+
+=head1 DESCRIPTION
+
+Typing C<-MLAAl> is slightly shorter than C<-MLAA='$log'>.
+
+=cut
