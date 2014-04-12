ea8a1531d5ddb4fcb47511cd1962a4b2877a75cd
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2014-04-12 09:26:14 +0700

    Add pointer on how to install the module

--- lib/SHARYANTO/Shortcuts.pm
+++ lib/SHARYANTO/Shortcuts.pm
@@ -8,7 +8,10 @@ package SHARYANTO::Shortcuts;
 =head1 DESCRIPTION
 
 This distribution contains shortcut names for various modules that I often use,
-mostly for one-liners. It does not reside on CPAN, for obvious reason.
+mostly for one-liners. It does not reside on CPAN, for obvious reason. Instead,
+I put it on my personal DarkPAN. You can install it with:
+
+ % cpanm --mirror https://github.com/sharyanto/darkpan-steven/raw/master/ --mirror-only SHARYANTO::Shortcuts
 
 
 =head1 SEE ALSO
